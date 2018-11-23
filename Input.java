0f75b67fdf65d36d35baf957693286f4c16484fe
Tasnim Ankon <tasnim.ankon@gmail.com>
2018-11-24 02:09:01 +0600

    listed down subsequences in single file

--- Input.java
+++ Input.java
@@ -2,6 +2,13 @@ public class Input {
     private int a;
     private int b;
 
+    public Input(int a) {
+        this.a = a;
+    }
+
+    public Input() {
+    }
+
     public Input(int a, int b) {
         this.a = a;
         this.b = b;
--- src/main/java/com/ankon/problem2/Main.java
+++ src/main/java/com/ankon/problem2/Main.java
@@ -2,11 +2,73 @@ package com.ankon.problem2;
 
 import com.ankon.problem2.lexer.Lexer;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static java.lang.System.in;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Lexer lexer = new Lexer("Input.java");
+        ArrayList<String> set = new ArrayList<>();
+
+        try {
+            String content = readFile("Input.java", Charset.defaultCharset());
+            set = getAllUniqueSubset(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (set != null && set.size() > 0) {
+            for (String string: set) {
+                // System.out.println(string);
+            }
+
+            HashMap<String, Integer> frequencyMap = new HashMap<String, Integer>();
+            for (String string: set) {
+                if (string.replaceAll("\\s", "").length() == 0)
+                    continue;
+                if (string.contains("\n"))
+                    continue;
+
+                if(frequencyMap.containsKey(string)) {
+                    frequencyMap.put(string, frequencyMap.get(string) + 1);
+                    System.out.println(string + " " + (frequencyMap.get(string)));
+                }
+                else {
+                    frequencyMap.put(string, 1);
+                    // System.out.println(string + " 1");
+                }
+            }
+        }
+    }
+
+    public static ArrayList<String> getAllUniqueSubset(String str) {
+        ArrayList<String> set = new ArrayList<String>();
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < str.length() - i; j++) {
+                String elem = str.substring(j, j + (i+1));
+//                if (!set.contains(elem)) {
+//                    set.add(elem);
+//                }
+                set.add(elem);
+            }
+        }
+        return set;
+    }
+
+    static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+    public static void lexFile(String path) {
+        Lexer lexer = new Lexer(path);
 
         System.out.println("Lexical Analysis");
         System.out.println("-----------------");
